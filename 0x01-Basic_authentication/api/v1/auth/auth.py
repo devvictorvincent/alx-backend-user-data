d3d57f181707be19ba4192efab83fc9514d82be9
Victor Vincent <dev.victovincent@gmail.com>
2024-08-07 17:21:57 +0000

    Auth Class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Auth module for the API
+"""
+from flask import request
+
+
+class Auth:
+    """
+    Auth Class
+    """
+    def require_auth(self, path: srt, excluded_paths: List[str]) -> bool:
+        """
+        Require Authentication
+        Returns:
+          - False - path and excluded_paths
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Authorization Header
+        Returns:
+          - None - request
+        """
+        return "None - request"
+
+    def def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Current User
+        Returns:
+          - None - request
+        """
