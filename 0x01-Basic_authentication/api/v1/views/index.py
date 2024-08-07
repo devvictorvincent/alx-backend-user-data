d6e70b665e6bc8f5bf220ab998db3e3373a49268
Victor Vincent <dev.victovincent@gmail.com>
2024-08-07 16:41:41 +0000

    Erro Handler: forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> None:
       - Error Handler
     """
     abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """ GET /api/vi/forbidden/
+      Return:
+        - Error Handler
+    """
+    abort(403)
