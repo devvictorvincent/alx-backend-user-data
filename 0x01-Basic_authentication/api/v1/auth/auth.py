a76f4dc5ddb7a6072e7425c2906122415fdf915b
Victor Vincent <dev.victovincent@gmail.com>
2024-08-07 19:15:04 +0000

    Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -41,6 +41,8 @@ class Auth:
         Returns:
           - None - request
         """
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
