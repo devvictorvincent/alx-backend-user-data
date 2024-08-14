29487218e08983767d6edeb1dd1e4a2500378e7c
Victor Vincent <dev.victovincent@gmail.com>
2024-08-14 20:56:28 +0000

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,12 @@ class BasicAuth(Auth):
     """
     Basic Auth Class
     """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        extract authorization
+        """
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startsqith("Basic "):
+            return None
+        return authorization_header[len("Basic "):]
