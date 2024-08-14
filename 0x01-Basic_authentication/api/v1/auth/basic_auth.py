648c10763b26e5c0b1a2b5d86eca863cbbdea7a0
Victor Vincent <dev.victovincent@gmail.com>
2024-08-14 21:28:49 +0000

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 Basic Authentication
 """
+import base64
 from .auth import Auth
 
 
@@ -21,3 +22,21 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Basic - Base64 decode
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64.b64decode(
+                    base64_authorization_header,
+                    validation=True,
+                    )
+            return decoded.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
